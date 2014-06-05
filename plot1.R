249d360e98ec0a03907e47f2e12fe531c073b5b9
Rosenschmidt <fred@rosenschmidt.org>
2014-06-05 00:17:22 -0500

    another typo

--- plot1.R
+++ plot1.R
@@ -2,7 +2,7 @@
 
 dt <- read.table("household_power_consumption.txt",sep=";",header=TRUE,na.string="?")
 
-dt$Date <- as.date(dt$Date,format="%m/%d/%Y")
+dt$Date <- as.Date(dt$Date,format="%d/%m/%Y")
 
 dt1 <- dt[dt$Date == "2007-02-01"]
 dt2 <- dt[dt$Date == "2007-02-02"]
