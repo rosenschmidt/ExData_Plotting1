063a272c7219824a3a6ade7114d14ef33ee4b161
Rosenschmidt <fred@rosenschmidt.org>
2014-06-05 08:09:33 -0500

    This is basically correct now

--- plot1.R
+++ plot1.R
@@ -1,13 +1,20 @@
 # plot1.R
 
-dt <- read.table("household_power_consumption.txt",sep=";",header=TRUE,na.string="?")
+dt <- read.table("household_power_consumption.txt",
+   sep=";",
+   header=TRUE,
+   na.string="?")
 
-dt$Date <- as.Date(dt$Date,format="%d/%m/%Y")
+# doesn't work - tale of woe
+# dt$Date <- as.Date(dt$Date,format="%d/%m/%Y")
 
-dt1 <- dt[dt$Date == "2007-02-01",]
-dt2 <- dt[dt$Date == "2007-02-02",]
-
+dt1 <- dt[dt$Date == "1/2/2007",]
+dt2 <- dt[dt$Date == "2/2/2007",]
 dtFeb12 <- rbind(dt1,dt2)
 
-hist(dtFeb12$Global_active_power)
+theDates <- paste(dtFeb12$Date,dtFeb12$Time)
+theDates <- strptime(theDates,"%d/%m/%Y %H:%M:%S",tz="")
 
+hist(dtFeb12$Global_active_power,
+   main="Global Active Power",
+   xlab="Global Active Power (kilowatts)")
